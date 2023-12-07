99e2a4c5f8e747fa8bed1b5e1c05bfcd0d550099
Florian Wiesner <florian.wiesner@avt.rwth-aachen.de>
2023-12-07 17:43:28 +0100

    working LBM sim

--- lbm_wetting/2_phase_sim.py
+++ lbm_wetting/2_phase_sim.py
@@ -1,7 +1,6 @@
 import subprocess
-import numpy as np
-import matplotlib.pyplot as plt
 import mplbm_utils as mplbm
+from pathlib import Path
 import structure_prep as prep
 
 
@@ -12,6 +11,7 @@ def run_2_phase_sim(inputs):
     # 3) run 2-phase sim
 
     sim_directory = inputs["input output"]["simulation directory"]
+    input_dir = inputs["input output"]["input folder"]
 
     # 1) Create Palabos geometry
     print("Creating efficient geometry for Palabos...")
@@ -21,85 +21,23 @@ def run_2_phase_sim(inputs):
 
     # 2) Create simulation input file
     print("Creating input file...")
-    mplbm.create_palabos_input_file(inputs)
+    palabos_file = prep.PalabosInputFile(inputs)
+    palabos_file.create_input_file()
 
     # 3) Run 2-phase simulation
     print("Running 2-phase simulation...")
     num_procs = inputs["simulation"]["num procs"]
-    input_dir = inputs["input output"]["input folder"]
-    simulation_command = f"mpirun -np {num_procs} ../../src/2-phase_LBM/ShanChen {input_dir}2_phase_sim_input.xml"
-    file = open(f"{sim_directory}/{input_dir}run_shanchen_sim.sh", "w")
-    file.write(f"{simulation_command}")
-    file.close()
 
-    simulation_command_subproc = f"bash {sim_directory}/{input_dir}run_shanchen_sim.sh"
-    subprocess.run(simulation_command_subproc.split(" "))
+    input_folder = Path(sim_directory) / input_dir
+    input_file = input_folder / "2_phase_sim_input.xml"
 
-    return
+    palabos_bin = Path(
+        "/home/fw641779/Coding/lattice-boltzmann-wetting/mplbm-ut-mirror/src/2-phase_LBM/ShanChen"
+    )
 
-
-def run_rel_perm_sim(inputs):
-    # Rel Perm Steps
-    # 1) create 1-phase palabos input file for relperms
-    # 2) create geoms for rel perm
-    # 3) run 1-phase sim to get rel perms
-
-    sim_directory = inputs["input output"]["simulation directory"]
-
-    # 1) Create geoms for rel perm
-    print("Creating rel perm geometries...")
-    user_geom_name = inputs["domain"]["geom name"]
-    inputs = mplbm.create_geom_for_rel_perm(inputs)
-
-    # 2) Create simulation input file
-    print("Creating input file...")
-    inputs["simulation type"] = "rel perm"
-    mplbm.create_palabos_input_file(inputs)
-    inputs["domain"]["geom name"] = user_geom_name
-
-    # 3) Write rel perm bash file
-    print("Running rel perm simulation...")
-    num_procs = inputs["simulation"]["num procs"]
-    input_dir = inputs["input output"]["input folder"]
-    output_dir = inputs["input output"]["output folder"]
-    simulation_command = f"mpirun -np {num_procs} ../../src/1-phase_LBM/permeability {input_dir}relperm_input.xml"
-    file = open(f"{sim_directory}/{input_dir}run_relperm_sim.sh", "w")
-    file.write(f"{simulation_command}")
-    file.close()
-
-    simulation_command_subproc = f"bash {sim_directory}/{input_dir}run_relperm_sim.sh"
-    make_4relperm_folder = f"mkdir {sim_directory}/{output_dir}/4relperm"
-    subprocess.run(make_4relperm_folder.split(" "))
-    subprocess.run(simulation_command_subproc.split(" "))
-
-    return
-
-
-def process_and_plot_results(inputs):
-    # Process data
-    mplbm.create_pressure_data_file(inputs)
-    mplbm.create_relperm_data_file(inputs)
-
-    # Load and plot data
-    sim_dir = inputs["input output"]["simulation directory"] + "/"
-    output_dir = inputs["input output"]["output folder"]
-    Sw = np.loadtxt(f"{sim_dir + output_dir}data_Sw.txt")
-    Pc = np.loadtxt(f"{sim_dir + output_dir}data_Pc.txt")
-    krw = np.loadtxt(f"{sim_dir + output_dir}data_krw.txt")
-    krnw = np.loadtxt(f"{sim_dir + output_dir}data_krnw.txt")
-
-    plt.figure(figsize=[10, 8])
-    mplbm.plot_capillary_pressure_data(Sw, Pc)
-    plt.savefig(sim_dir + "pc_curve.png", dpi=300)
-
-    plt.figure(figsize=[10, 8])
-    mplbm.plot_rel_perm_data(Sw, krw, krnw)
-    # plt.yscale('log')
-    plt.savefig(sim_dir + "relperm_curve.png", dpi=300)
-
-    plt.figure(figsize=[6, 10])
-    mplbm.plot_pc_and_rel_perm(Sw, Pc, krw, krnw)
-    plt.savefig(sim_dir + "pc_and_relperm_curve.png", dpi=300)
+    subprocess.run(
+        ["mpirun", "-np", f"{num_procs}", f"{palabos_bin}", f"{input_file}"], check=True
+    )
 
     return
 
--- lbm_wetting/structure_prep.py
+++ lbm_wetting/structure_prep.py
@@ -1,16 +1,15 @@
 from pathlib import Path
 import numpy as np
-from skimage import measure
 import scipy.ndimage.morphology
 
-class PalabosGeometry:
 
+class PalabosGeometry:
     def __init__(self, inputs):
         self.sim_dir = inputs["input output"]["simulation directory"]
         self.input_dir = inputs["input output"]["input folder"]
 
         self.geom_file_name = inputs["geometry"]["file name"]
-        self.geom_file = Path(self.sim_dir) / self.input_dir  / self.geom_file_name
+        self.geom_file = Path(self.sim_dir) / self.input_dir / self.geom_file_name
         self.geom_name = inputs["domain"]["geom name"]
         self.inout_layers = inputs["domain"]["inlet and outlet layers"]
 
@@ -38,7 +37,6 @@ class PalabosGeometry:
         nw_fluid[structure == 3] = 3
         return nw_fluid
 
-
     def _remove_nw_fluid(self, structure: np.ndarray) -> np.ndarray:
         """
         Removes the non-wetting fluid from a structure
@@ -56,7 +54,6 @@ class PalabosGeometry:
         """
         structure[structure == 3] = 0
         return structure
-
 
     def _get_surface(self, structure: np.ndarray) -> np.ndarray:
         """
@@ -79,7 +76,6 @@ class PalabosGeometry:
         )
         surface = np.logical_and(distance_map > 0, distance_map < 2)
         return surface
-
 
     def _get_solid(self, structure: np.ndarray) -> np.ndarray:
         """
@@ -105,7 +101,9 @@ class PalabosGeometry:
 
         return solid
 
-    def _add_inlet_outlet_layers(self, structure: np.ndarray, num_layers: int) -> np.ndarray:
+    def _add_inlet_outlet_layers(
+        self, structure: np.ndarray, num_layers: int
+    ) -> np.ndarray:
         """
         Adds inlet and outlet layers to a structure
 
@@ -122,10 +120,13 @@ class PalabosGeometry:
             The structure array with inlet and outlet layers added
 
         """
-        structure = np.pad(structure, [(num_layers, num_layers), (0, 0), (0, 0)],
-                        mode="constant", constant_values=(0, 0))
+        structure = np.pad(
+            structure,
+            [(num_layers, num_layers), (0, 0), (0, 0)],
+            mode="constant",
+            constant_values=(0, 0),
+        )
         return structure
-
 
     def convert_material_ids(self, conversion: dict = None):
         """Convert the material ids (255, 200 etc) to the correct values for Palabos"""
@@ -135,14 +136,13 @@ class PalabosGeometry:
                 255: 1,
                 200: 4,
                 150: 6,
-                128: 3, # This is the non-wetting fluid
+                128: 3,  # This is the non-wetting fluid
             }
 
         for key, val in conversion.items():
             self.structure[self.structure == key] = val
 
     def create_geom_for_palabos(self):
-
         # Create a mask for the non-wetting fluid
         nw_fluid_mask = self._extract_nw_fluid(self.structure)
         # Remove the non-wetting fluid from the structure
@@ -167,3 +167,187 @@ class PalabosGeometry:
 
         self.structure.flatten().tofile(path / f"{self.geom_name}.dat")  # Save geometry
         np.save(path / f"{self.geom_name}.npy", self.structure)
+
+
+class PalabosInputFile:
+    def __init__(self, inputs):
+        self.inputs = inputs
+
+        sim_dir = inputs["input output"]["simulation directory"]
+        input_dir = inputs["input output"]["input folder"]
+        output_folder = inputs["input output"]["output folder"]
+
+        self.output_dir = Path(sim_dir) / output_folder
+
+        self.file = Path(sim_dir) / input_dir / "2_phase_sim_input.xml"
+
+        restart_sim = inputs["simulation"]["restart sim"]
+        with open(self.file, "w") as file:
+            file.write('<?xml version="1.0"?>\n\n')
+            file.write(f"<load_savedstated> {restart_sim} </load_savedstated>\n\n")
+
+    def _write_geometry_section(self):
+        # Get geometry inputs
+        periodic_x = self.inputs["domain"]["periodic boundary"]["x"]
+        periodic_y = self.inputs["domain"]["periodic boundary"]["y"]
+        periodic_z = self.inputs["domain"]["periodic boundary"]["z"]
+
+        nx = self.inputs["domain"]["domain size"]["nx"]
+        ny = self.inputs["domain"]["domain size"]["ny"]
+        nz = self.inputs["domain"]["domain size"]["nz"]
+        num_layers = self.inputs["domain"]["inlet and outlet layers"]
+        domain_size = [nx + (2 * num_layers), ny, nz]
+
+        geo_file_name = self.inputs["domain"]["geom name"]
+        geo_file = self.file.parent / f"{geo_file_name}.dat"
+
+        with open(self.file, "a") as file:
+            # Write geometry section
+            file.write("<geometry>\n")
+            # Geometry name
+            file.write(f"\t<file_geom> {geo_file} </file_geom>\n")
+            # Geometry size
+            file.write(
+                f"\t<size> <x> {domain_size[0]} </x> <y> {domain_size[1]} </y> <z> {domain_size[2]} </z> </size>\n"
+            )
+            # Periodicity
+            file.write(f"\t<per>\n")
+            file.write(
+                f"\t\t<fluid1> <x> {periodic_x} </x> <y> {periodic_y} </y> <z> {periodic_z} </z> </fluid1>\n"
+            )
+            file.write(
+                f"\t\t<fluid2> <x> {periodic_x} </x> <y> {periodic_y} </y> <z> {periodic_z} </z> </fluid2>\n"
+            )
+            file.write(f"\t</per>\n")
+            file.write("</geometry>\n\n")
+
+    def _write_fluid_positions(self):
+        load_fluid_type = self.inputs["simulation"]["fluid init"]
+        if load_fluid_type == "geom":
+            load_fluid_from_geom = True
+        else:
+            load_fluid_from_geom = False
+
+        fluid1_x1 = self.inputs["simulation"]["fluid 1 init"]["x1"]
+        fluid1_x2 = self.inputs["simulation"]["fluid 1 init"]["x2"]
+        fluid1_y1 = self.inputs["simulation"]["fluid 1 init"]["y1"]
+        fluid1_y2 = self.inputs["simulation"]["fluid 1 init"]["y2"]
+        fluid1_z1 = self.inputs["simulation"]["fluid 1 init"]["z1"]
+        fluid1_z2 = self.inputs["simulation"]["fluid 1 init"]["z2"]
+        fluid2_x1 = self.inputs["simulation"]["fluid 2 init"]["x1"]
+        fluid2_x2 = self.inputs["simulation"]["fluid 2 init"]["x2"]
+        fluid2_y1 = self.inputs["simulation"]["fluid 2 init"]["y1"]
+        fluid2_y2 = self.inputs["simulation"]["fluid 2 init"]["y2"]
+        fluid2_z1 = self.inputs["simulation"]["fluid 2 init"]["z1"]
+        fluid2_z2 = self.inputs["simulation"]["fluid 2 init"]["z2"]
+
+        with open(self.file, "a") as file:
+            # Write initial position of fluids
+            file.write(f"<init>\n")
+            file.write(
+                f"\t<fluid_from_geom> {load_fluid_from_geom} </fluid_from_geom>\n"
+            )
+            file.write(f"\t<fluid1>\n")
+            file.write(
+                f"\t\t <x1> {fluid1_x1} </x1> <y1> {fluid1_y1} </y1> <z1> {fluid1_z1} </z1>\n"
+            )
+            file.write(
+                f"\t\t <x2> {fluid1_x2} </x2> <y2> {fluid1_y2} </y2> <z2> {fluid1_z2} </z2>\n"
+            )
+            file.write(f"\t</fluid1>\n")
+            file.write(f"\t<fluid2>\n")
+            file.write(
+                f"\t\t <x1> {fluid2_x1} </x1> <y1> {fluid2_y1} </y1> <z1> {fluid2_z1} </z1>\n"
+            )
+            file.write(
+                f"\t\t <x2> {fluid2_x2} </x2> <y2> {fluid2_y2} </y2> <z2> {fluid2_z2} </z2>\n"
+            )
+            file.write(f"\t</fluid2>\n")
+            file.write("</init>\n\n")
+
+    def _write_fluid_data(self):
+        rho_f1 = self.inputs["simulation"]["rho_f1"]
+        rho_f2 = self.inputs["simulation"]["rho_f2"]
+
+        force_f1 = self.inputs["simulation"]["force_f1"]
+        force_f2 = self.inputs["simulation"]["force_f2"]
+
+        Gc = self.inputs["simulation"]["fluid data"]["Gc"]
+        omega_f1 = self.inputs["simulation"]["fluid data"]["omega_f1"]
+        omega_f2 = self.inputs["simulation"]["fluid data"]["omega_f2"]
+        G_ads_f1_s1 = self.inputs["simulation"]["fluid data"]["G_ads_f1_s1"]
+        G_ads_f1_s2 = self.inputs["simulation"]["fluid data"]["G_ads_f1_s2"]
+        G_ads_f1_s3 = self.inputs["simulation"]["fluid data"]["G_ads_f1_s3"]
+        G_ads_f1_s4 = self.inputs["simulation"]["fluid data"]["G_ads_f1_s4"]
+
+        pressure_bc = self.inputs["simulation"]["pressure bc"]
+        if pressure_bc == True:
+            minimum_radius = self.inputs["simulation"]["minimum radius"]
+            num_pc_steps = self.inputs["simulation"]["num pressure steps"]
+        else:
+            minimum_radius = 1
+            num_pc_steps = 0
+
+        with open(self.file, "a") as file:
+            # Write fluid data
+            file.write("<fluids>\n")
+
+            file.write(f"\t<Gc> {Gc} </Gc>\n")
+            file.write(f"\t<omega_f1> {omega_f1} </omega_f1>\n")
+            file.write(f"\t<omega_f2> {omega_f2} </omega_f2>\n")
+            file.write(f"\t<force_f1> {force_f1} </force_f1>\n")
+            file.write(f"\t<force_f2> {force_f2} </force_f2>\n")
+            file.write(f"\t<G_ads_f1_s1> {G_ads_f1_s1} </G_ads_f1_s1>\n")
+            file.write(f"\t<G_ads_f1_s2> {G_ads_f1_s2} </G_ads_f1_s2>\n")
+            file.write(f"\t<G_ads_f1_s3> {G_ads_f1_s3} </G_ads_f1_s3>\n")
+            file.write(f"\t<G_ads_f1_s4> {G_ads_f1_s4} </G_ads_f1_s4>\n")
+
+            file.write(f"\t<rho_f1> {rho_f1} </rho_f1>\n")
+            file.write(f"\t<rho_f2> {rho_f2} </rho_f2>\n")
+
+            file.write(f"\t<pressure_bc> {pressure_bc} </pressure_bc>\n")
+            file.write(f"\t<rho_f1_i> {rho_f1} </rho_f1_i>\n")
+            file.write(f"\t<rho_f2_i> {rho_f2} </rho_f2_i>\n")
+            file.write(f"\t<num_pc_steps> {num_pc_steps} </num_pc_steps>\n")
+            file.write(f"\t<min_radius> {minimum_radius} </min_radius>\n")
+            file.write(f"\t<rho_d> 0.06 </rho_d>\n")
+
+            file.write("</fluids>\n\n")
+
+    def _write_output_section(self):
+        convergence = self.inputs["simulation"]["convergence"]
+        convergence_iter = self.inputs["simulation"]["convergence iter"]
+        max_iter = self.inputs["simulation"]["max iterations"]
+        save_sim = self.inputs["simulation"]["save sim"]
+        save_iter = self.inputs["simulation"]["save iter"]
+        gif_iter = self.inputs["simulation"]["gif iter"]
+        vtk_iter = self.inputs["simulation"]["vtk iter"]
+        rho_f2_vtk = self.inputs["simulation"]["rho_f2_vtk"]
+        print_geom = self.inputs["simulation"]["print geom"]
+        print_stl = self.inputs["simulation"]["print stl"]
+
+        with open(self.file, "a") as file:
+            # Write output section
+            file.write("<output>\n")
+
+            # The / after the {dir} is important for the c++ code
+            file.write(f"\t<out_folder> {self.output_dir}/ </out_folder>\n")
+
+            file.write(f"\t<save_it> {save_iter} </save_it>\n")
+            file.write(f"\t<save_sim> {save_sim} </save_sim>\n")
+            file.write(f"\t<convergence> {convergence} </convergence>\n")
+            file.write(f"\t<it_max> {max_iter} </it_max>\n")
+            file.write(f"\t<it_conv> {convergence_iter} </it_conv>\n")
+            file.write(f"\t<it_gif> {gif_iter} </it_gif>\n")
+            file.write(f"\t<rho_vtk> {rho_f2_vtk} </rho_vtk>\n")
+            file.write(f"\t<it_vtk> {vtk_iter} </it_vtk>\n")
+            file.write(f"\t<print_geom> {print_geom} </print_geom>\n")
+            file.write(f"\t<print_stl> {print_stl} </print_stl>\n")
+
+            file.write("</output>")
+
+    def create_input_file(self):
+        self._write_geometry_section()
+        self._write_fluid_positions()
+        self._write_fluid_data()
+        self._write_output_section()
