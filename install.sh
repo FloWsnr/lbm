d82762a4a50be865763a3b33daf4b3b86040cc9d
Florian Wiesner <florian.wiesner@avt.rwth-aachen.de>
2025-05-27 20:19:59 +0200

    update install

--- install.sh
+++ install.sh
@@ -6,10 +6,13 @@ module load CMake/3.24.3
 cd ./palabos_sim/src
 unzip palabos-v2.2.1.zip
 
+mkdir -p 2-phase_LBM/build
 cd 2-phase_LBM/build
 cmake ..
 make -j 2
 
-cd ../../1-phase_LBM/build
+cd ../../1-phase_LBM
+mkdir -p build
+cd build
 cmake ..
 make -j 2
